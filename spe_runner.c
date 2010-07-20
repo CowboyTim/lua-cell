81be93c9c5aca0c81ad411a5c0557cc7ce9b364f
CowboyTim <aardbeiplantje@gmail.com>
2010-07-20 23:21:12 +0200

    - pad to 4 in the spe <-> spe runner channel

--- spe_runner.c
+++ spe_runner.c
@@ -7,40 +7,52 @@
 
 int main(unsigned long long spe, unsigned long long argp, unsigned long long envp) {
     lua_State *L = (lua_State *)&argp;
-    int i, j;
+    int r0, r1, r2, r3, r4;
     {
         /*  opcode: 4 (GETUPVAL)   a:0,b:0,c:0     */
         spu_write_out_intr_mbox(OP_GETUPVAL);
         spu_write_out_intr_mbox(0);
-        i = spu_read_in_mbox();
-        fprintf(stderr, "upval 0: %d\n", i);
+        spu_write_out_intr_mbox(0);
+        spu_write_out_intr_mbox(0);
+        r0 = spu_read_in_mbox();
+        fprintf(stderr, "upval 0: %d\n", r0);
 
         /*  opcode: 12 (ADD)       a:0,b:0,c:256   */  
-        i = i + 10;
+        r0 = r0 + 10;
 
         /*  opcode: 4 (GETUPVAL)   a:1,b:1,c:0     */
         spu_write_out_intr_mbox(OP_GETUPVAL);
         spu_write_out_intr_mbox(1);
-        j = spu_read_in_mbox();
-        fprintf(stderr, "upval 1: %d\n", j);
+        spu_write_out_intr_mbox(0);
+        spu_write_out_intr_mbox(0);
+        r1 = spu_read_in_mbox();
+        fprintf(stderr, "upval 1: %d\n", r1);
 
         /*  opcode: 1 (LOADK)      a:2,b:1,c:<nop> */
 
         /*  opcode: 4 (GETUPVAL)   a:2,b:2,c:0     */
         spu_write_out_intr_mbox(OP_GETUPVAL);
         spu_write_out_intr_mbox(2);
-        j = spu_read_in_mbox();
-        fprintf(stderr, "upval 2: %d\n", j);
+        spu_write_out_intr_mbox(0);
+        spu_write_out_intr_mbox(0);
+        r2 = spu_read_in_mbox();
+        fprintf(stderr, "upval 2: %d\n", r2);
 
         /*  opcode: 6 (GETTABLE)   a:1,b:1,c:2     */
-        /*  opcode: 5 (GETGLOBAL)  a:3,b:2,c:<nop> */
+        /*  opcode: 5 (GETGLOBAL)  a:4,b:2,c:<nop> */
         spu_write_out_intr_mbox(OP_GETGLOBAL);
         spu_write_out_intr_mbox(1);
-        j = spu_read_in_mbox();
+        spu_write_out_intr_mbox(0);
+        spu_write_out_intr_mbox(0);
+        r4 = spu_read_in_mbox();
     }
     spu_write_out_intr_mbox(999);
     spu_write_out_intr_mbox(0);
+    spu_write_out_intr_mbox(0);
+    spu_write_out_intr_mbox(0);
     spu_write_out_intr_mbox(LUA_TNUMBER);
-    spu_write_out_intr_mbox(i);
+    spu_write_out_intr_mbox(r0);
+    spu_write_out_intr_mbox(0);
+    spu_write_out_intr_mbox(0);
     return 0;
 }
